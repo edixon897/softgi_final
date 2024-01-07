755db11bc096d1961409240b53554af027adad44
edixon897 <edixonpayan5@gmail.com>
2024-01-07 11:18:19 -0600

    proveedores ok

--- models/proveedores.py
+++ models/proveedores.py
@@ -9,14 +9,13 @@ class Proveedores:
         
     #Crear provedores
     def crear(self, agregar):
-
         bsdsql = f" INSERT INTO `proveedores`(`doc_proveedor`, `nom_proveedor`, `contacto_proveedor`, `email_proveedor`, `direccion_proveedor`, `ciudad_proveedor`, `registro_proveedor`, `documento_operador`,  `nombre_operador`, `apellido_operador`, `estado_proveedor` ) VALUES ('{agregar[0]}','{agregar[1]}','{agregar[2]}','{agregar[3]}','{agregar[4]}', '{agregar[5]}', '{agregar[6]}', '{agregar[7]}', '{agregar[8]}', '{agregar[9]}','ACTIVO')"
         self.cursor.execute(bsdsql)
         self.conexion.commit()
 
     #modificar Proveedores
     def modificar(self,proveedor):
-        sql=f"UPDATE proveedores SET doc_proveedor='{proveedor[0]}',nom_proveedor='{proveedor[1]}',contacto_proveedor='{proveedor[2]}',email_proveedor='{proveedor[3]}', direccion_proveedor='{proveedor[4]}', ciudad_proveedor='{proveedor[5]}' WHERE doc_proveedor='{proveedor[0]}'"
+        sql=f"UPDATE proveedores SET doc_proveedor='{proveedor[0]}', nom_proveedor='{proveedor[1]}',contacto_proveedor='{proveedor[2]}', email_proveedor='{proveedor[3]}', direccion_proveedor='{proveedor[4]}', ciudad_proveedor='{proveedor[5]}', documento_operador='{proveedor[6]}', nombre_operador='{proveedor[7]}',apellido_operador='{proveedor[8]}' WHERE doc_proveedor='{proveedor[0]}'"
         self.cursor.execute(sql)
         self.conexion.commit()
 
--- routes/proveedores.py
+++ routes/proveedores.py
@@ -8,23 +8,64 @@ from models.proveedores import Dproveedores, Proveedores
 @app.route('/proveedores')
 def proveedores():
     if "nom_empleado" in session:
-        sql = "SELECT * FROM proveedores WHERE estado_proveedor = 'ACTIVO'"
+        sql = "SELECT `doc_proveedor`, `nom_proveedor`, `contacto_proveedor`, `email_proveedor`, `direccion_proveedor` FROM `proveedores` WHERE estado_proveedor = 'ACTIVO'"
         conn = mysql.connect()
         cursor = conn.cursor()
         cursor.execute(sql)
         resultado = cursor.fetchall()
-        return render_template('/proveedores/muestra_Proveedores.html', resulta = resultado)
+        return render_template('/proveedores/muestra_Proveedores.html', resul = resultado)
     else:
         flash('Por favor inicia sesion')
         return redirect(url_for('index'))
 
 
-@app.route('/crearProveedores', methods=['POST'])
-def crearProveedores():
+@app.route('/crearProveedores', methods=['POST', 'GET'])
+def crear_Proveedores():
+    if "nom_empleado" in session:
+        if request.method == 'POST':
+            doc = session["nom_empleado"]
+            bsq = f"SELECT `doc_empleado`, `nom_empleado`, `ape_empleado` FROM empleados WHERE nom_empleado='{doc}'"
+            print(bsq)
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(bsq)
+            resultado = cursor.fetchone()
+            print(resultado)
+            documento_registro = resultado[0]
+            nombre_operador = resultado[1]
+            apellido_operador = resultado[2]
+            documento = request.form['documentoProveedor']
+            nombre = request.form['nombreProveedor']
+            numero = request.form['numeroProveedores']
+            correo = request.form['correoProveedores']
+            direcion = request.form['direccionProveedores']
+            ciudad = request.form['ciudadProveedor']
+            tiempo = datetime.datetime.now()
+
+            Dproveedores.crear([documento,nombre,numero,correo,direcion,ciudad, tiempo, documento_registro, nombre_operador, apellido_operador])
+            return redirect('/proveedores')
+        else:
+            return render_template('proveedores/crear_proveedores.html')
+    else:
+        flash('Algo está mal en los datos digitados')
+        return redirect(url_for('home'))
+    
+    
+@app.route('/actializarProveedor/<proveedor>', methods=['POST', 'GET'])
+def actializar_Proveedor(proveedor):
+    if "nom_empleado" in session:
+        sql= f"SELECT `doc_proveedor`, `nom_proveedor`, `contacto_proveedor`, `email_proveedor`, `direccion_proveedor`, `ciudad_proveedor` FROM `proveedores` WHERE doc_proveedor = '{proveedor}'"
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        resultado = cursor.fetchone()
+        return render_template('proveedores/actualizar_proveedores.html', resul = resultado)
+
+@app.route('/actializarProveed', methods=['POST', 'GET'])
+def actializar_Proveed():
     if "nom_empleado" in session:
         doc = session["nom_empleado"]
-        bsq = f"SELECT `doc_empleado`, `nom_empleado`, `ape_empleado` FROM empleados WHERE email_empleado='{doc}'"
-        print(bsq)
+        bsq = f"SELECT `doc_empleado`, `nom_empleado`, `ape_empleado` FROM empleados WHERE nom_empleado='{doc}'"
         conn = mysql.connect()
         cursor = conn.cursor()
         cursor.execute(bsq)
@@ -39,49 +80,17 @@ def crearProveedores():
         correo = request.form['correoProveedores']
         direcion = request.form['direccionProveedores']
         ciudad = request.form['ciudadProveedor']
-        tiempo = datetime.datetime.now()
+        Dproveedores.modificar([documento,nombre,numero,correo,direcion,ciudad, documento_registro, nombre_operador, apellido_operador])
+        return redirect('/proveedores')
 
-        Dproveedores.crear([documento,nombre,numero,correo,direcion,ciudad, tiempo, documento_registro, nombre_operador, apellido_operador])
-        return redirect('/muestra_Proveedores')
-    else:
-        flash('Algo está mal en los datos digitados')
-        return redirect(url_for('home'))
-    
-    
-#-----------------------------------------------------------mostrar proveedores---------------------------------------
 
-@app.route('/muestra_Proveedores')
-def muestra_Proveedores():
+
+
+@app.route('/borraProveedor/<documento>')
+def borrarProveedor(documento):
     if "nom_empleado" in session:
-        sql = "SELECT * FROM `proveedores` WHERE estado_proveedor='ACTIVO'"           # consulta toda la info de proveedores.
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute(sql)
-        resultado = cursor.fetchall()   # no me toque el codigo niche pleace
-        conn.commit()
-        if (len(resultado) >= 1):
-            return render_template("/provedor/muestra_proveedores.html", resul=resultado)   # si hay resultados se muestran.
-        else:
-            resultado2 = "No hay proveedores registrados"
-            return render_template("/provedor/muestra_proveedores.html", resul2=resultado2)
-    # sino se muestra el mensaje de resultado2.
+        Dproveedores.borrar(documento)                 
+        return redirect("/proveedores")
     else:
-        flash('Algo está mal en los datos digitados')
-        return redirect(url_for('home'))
-
-@app.route('/buscarProvedores', methods=['POST', 'GET'])
-def buscarProvedores():
-    if request.method == 'POST':
-        # Get the search term from the form
-        buscar = request.form['buscarProvedor']
-
-        # Query the database
-        conn = mysql.connect()
-        cursor = conn.cursor() 
-        cursor.execute("SELECT * FROM proveedores WHERE estado_proveedor = %s AND nom_proveedor LIKE %s", ('ACTIVO', '%' + buscar + '%',))
-        results = cursor.fetchall()
-        cursor.close()
-
-        return render_template('provedor/buscarProvedor.html', results=results)
-
-    return render_template('provedor/buscarProvedor.html')
+        flash('Porfavor inicia sesion para poder acceder')
+        return redirect(url_for('index'))
