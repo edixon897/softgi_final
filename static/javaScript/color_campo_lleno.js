f41d8039bd267817c6579f82bd183b3821e47548
dennis1236 <dennisecheverri46@gmail.com>
2024-01-12 18:30:43 -0500

    Frond-End de registrar cliente Completado

---
+++ static/javaScript/color_campo_lleno.js
@@ -0,0 +1,106 @@
+function campo_lleno1() {
+    var documento = document.getElementById("documento");
+    var valor = documento.value;
+
+    if (valor.length > 0) {
+        documento.style.border = "1px solid #05c673";
+    } else {
+        documento.style.border = "1px solid black";
+    }
+}
+
+
+function campo_lleno2() {
+    var nombre = document.getElementById("nombre");
+    var valor = nombre.value;
+
+    if (valor.length > 0) {
+        nombre.style.border = "1px solid #05c673";
+    } else {
+        nombre.style.border = "1px solid black";
+    }
+}
+
+
+function campo_lleno3() {
+    var apellido = document.getElementById("apellido");
+    var valor = apellido.value;
+
+    if (valor.length > 0) {
+        apellido.style.border = "1px solid #05c673";
+    } else {
+        apellido.style.border = "1px solid black";
+    }
+}
+
+
+function campo_lleno4() {
+    var fecha = document.getElementById("fecha");
+    var valor = fecha.value;
+
+    if (valor.length > 0) {
+        fecha.style.border = "1px solid #05c673";
+    } else {
+        fecha.style.border = "1px solid black";
+    }
+}
+
+
+function campo_lleno5() {
+    var opt = document.getElementById("opt");
+    var valor = opt.value;
+
+    if (valor.length > 0) {
+        opt.style.border = "1px solid #05c673";
+    } else {
+        opt.style.border = "1px solid black";
+    }
+}
+
+
+function campo_lleno6() {
+    var contacto = document.getElementById("contacto");
+    var valor = contacto.value;
+
+    if (valor.length > 0) {
+        contacto.style.border = "1px solid #05c673";
+    } else {
+        contacto.style.border = "1px solid black";
+    }
+}
+
+
+function campo_lleno7() {
+    var correo = document.getElementById("correo");
+    var valor = correo.value;
+
+    if (valor.length > 0) {
+        correo.style.border = "1px solid #05c673";
+    } else {
+        correo.style.border = "1px solid black";
+    }
+}
+
+
+function campo_lleno8() {
+    var direccion = document.getElementById("direccion");
+    var valor = direccion.value;
+
+    if (valor.length > 0) {
+        direccion.style.border = "1px solid #05c673";
+    } else {
+        direccion.style.border = "1px solid black";
+    }
+}
+
+
+function campo_lleno9() {
+    var direccion = document.getElementById("ciudad");
+    var valor = ciudad.value;
+
+    if (valor.length > 0) { 
+        ciudad.style.border = "1px solid #05c673";
+    } else {
+        ciudad.style.border = "1px solid black";
+    }
+}
