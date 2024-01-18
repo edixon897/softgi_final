d68de3452f5eee2e4e214878541cbc0fb44a6fa5
dennis1236 <dennisecheverri46@gmail.com>
2024-01-18 13:15:23 -0500

    FRONT-END edita_productos completado

--- static/javaScript/sesiones_formulario_edita_productos.js
+++ static/javaScript/sesiones_formulario_edita_productos.js
@@ -78,5 +78,208 @@ function cambia_color_1() {
         label_1.style.color = "#358CB4"
     }
     
+}
+
+
+function cambia_color_2() {
+    let referencia_producto = document.getElementById('ref_produ_2');
+    let label_2 = document.getElementById('label_2')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label_2.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label_2.style.color = "#358CB4"
+    }
+    
+}
+
+
+function cambia_color_3() {
+    let referencia_producto = document.getElementById('ref_produ_3');
+    let label = document.getElementById('label_3')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }
+    
+}
+
+
+function cambia_color_4() {
+    let referencia_producto = document.getElementById('nom_proveedor');
+    let label = document.getElementById('label_4')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }
+    
+}
+
+
+function cambia_color_5() {
+    let referencia_producto = document.getElementById('nom_categoria');
+    let label = document.getElementById('label_5')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }
+    
+}
+
+
+function cambia_color_6() {
+    let referencia_producto = document.getElementById('nombre_producto');
+    let label = document.getElementById('label_6')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }
+    
+}
+
+
+function cambia_color_7() {
+    let referencia_producto = document.getElementById('precio_compra');
+    let label = document.getElementById('label_7')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }
+    
+}
+
+
+function cambia_color_8() {
+    let referencia_producto = document.getElementById('precio_venta');
+    let label = document.getElementById('label_8')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }
+    
+}
+
+
+function cambia_color_9() {
+    let referencia_producto = document.getElementById('descripcion');
+    let label = document.getElementById('label_9')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }
+    
+}
+
+
+function cambia_color_10() {
+    let referencia_producto = document.getElementById('cantidad_producto');
+    let label = document.getElementById('label_10')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }
+    
+}
+
+
+function cambia_color_11() {
+    let referencia_producto = document.getElementById('stockminimo');
+    let label = document.getElementById('label_11')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }
+    
+}
+
+
+function cambia_color_12() {
+    let referencia_producto = document.getElementById('ubicacion');
+    let label = document.getElementById('label_12')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }
+    
+}
+
+
+function cambia_color_13() {
+    let referencia_producto = document.getElementById('estante');
+    let label = document.getElementById('label_13')
+
+    let longitud = referencia_producto.value;
+
+    if (longitud > 0) {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }else {
+        referencia_producto.style.border = "1px solid #358CB4"
+        label.style.color = "#358CB4"
+    }
     
 }
