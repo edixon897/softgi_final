943e61bebe9bf1c5aaa73c2d1720d40770f752a7
dennis1236 <dennisecheverri46@gmail.com>
2024-01-29 21:30:55 -0500

    productos listo

--- static/javaScript/sesiones_formulario_registro_productos.js
+++ static/javaScript/sesiones_formulario_registro_productos.js
@@ -73,36 +73,55 @@ function animacion_envio() {
 
     /* let proveedor = document.getElementById('proveedor').value; */
     let nombre_producto = document.getElementById('nombre_producto').value;
-    let precio_venta = document.getElementById('precio_venta').value;
-    let cantidad_producto = document.getElementById('cantidad_producto').value;
     let ubicacion = document.getElementById('ubicacion').value;
+
+    /* todos los valores numericos capturados */
+    let precio_compra = document.getElementById('precio_compra');
+    let precio_venta = document.getElementById('precio_venta').value;
+
+    let cantidad_producto = document.getElementById('cantidad_producto').value;
+    let stockminimo = document.getElementById('stockminimo').value;
+
 
     form.addEventListener("submit", function(event) {
         event.preventDefault()
     });
 
 
-    if ((nombre_producto.length > 0) && (precio_venta.length > 0 ) && (cantidad_producto.length > 0) && (ubicacion.length > 0)) {
+    if ((precio_compra > 0) && (precio_venta > 0) && (cantidad_producto > 0) (stockminimo >= 0))  {
 
-        Swal.fire({
-            icon: "success",
-            text: "Registrando producto",
-            width: "42%",
-            height: "20%",
-            timer: 1000,
-            showConfirmButton: false
-        });
+    
+        if ((nombre_producto.length > 0) && (precio_venta.length > 0 ) && (precio_compra.length > 0) && (cantidad_producto.length > 0) && (stockminimo.length > 0) && (ubicacion.length > 0)) {
 
-        setTimeout(function() {
-            form.submit()
-        }, 1100)
+            Swal.fire({
+                icon: "success",
+                text: "Registrando producto",
+                width: "42%",
+                height: "20%",
+                timer: 1000,
+                showConfirmButton: false
+            });
 
+            setTimeout(function() {
+                form.submit()
+            }, 1100)
+
+        }else{
+
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "Faltan campos por completar",
+                width: "50%",
+                height: "20%",
+                showConfirmButton: true
+            });
+        }
     }else{
-
         Swal.fire({
             icon: "error",
             title: "Error",
-            text: "Faltan campos por completar",
+            text: "valor negativo degitado",
             width: "50%",
             height: "20%",
             showConfirmButton: true
