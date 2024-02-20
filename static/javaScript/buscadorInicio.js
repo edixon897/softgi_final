6f6c22ea00d541f987152452e31f2373101486d7
dennis1236 <dennisecheverri46@gmail.com>
2024-02-19 21:51:45 -0500

    roles

--- static/javaScript/buscadorInicio.js
+++ static/javaScript/buscadorInicio.js
@@ -20,4 +20,47 @@ function buscarEnTiempoReal() {
 };
 
 
-document.getElementById("searchProduct").addEventListener("input", buscarEnTiempoReal);
+
+
+function oculta_modulos() {
+    let productos = document.getElementById('modulo_productos');
+    let ventas = document.getElementById('modulo_ventas');
+    let cotizaciones = document.getElementById('modulo_cotizaciones');
+    let compras_prove = document.getElementById('modulo_compra_prove');
+    let modulo_clientes = document.getElementById('modulo_clientes');
+    let modulo_empleados = document.getElementById('modulo_empleados');
+
+
+    let input_oculto = document.getElementById('input_oculta_rol');
+    let valor_input = input_oculto.value;
+    console.log(valor_input)
+
+    if (valor_input == "administrador") {
+        productos.style.display = "block";
+        ventas.style.display = "block";
+        cotizaciones.style.display = "block";
+        compras_prove.style.display = "block";
+        modulo_clientes.style.display = "block";
+        modulo_empleados.style.display = "block";
+
+    } else if (valor_input == "vendedor") {
+        ventas.style.display = "block";
+        cotizaciones.style.display = "block";
+        modulo_clientes.style.display = "block";
+
+    } else if (valor_input == "almacenista") {
+        productos.style.display = "block";
+        compras_prove.style.display = "block";
+    }
+    
+
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Llama a la función para realizar la verificación inicial
+    oculta_modulos();
+    
+});
