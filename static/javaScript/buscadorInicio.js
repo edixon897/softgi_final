347be6566dfcd9bc5f84c46ac5980e9166fee9d7
dennis1236 <dennisecheverri46@gmail.com>
2024-03-09 16:18:15 -0500

    ..I..I..I..

--- static/javaScript/buscadorInicio.js
+++ static/javaScript/buscadorInicio.js
@@ -36,21 +36,66 @@ function oculta_modulos() {
     console.log(valor_input)
 
     if (valor_input == "administrador") {
+
         productos.style.display = "block";
+        setTimeout( function() {
+            productos.style.opacity = "1"
+        }, 25)
+
         ventas.style.display = "block";
+        setTimeout( function() {
+            ventas.style.opacity = "1"
+        }, 55)
+
         cotizaciones.style.display = "block";
+        setTimeout( function() {
+            cotizaciones.style.opacity = "1"
+        }, 85)
+
         compras_prove.style.display = "block";
+        setTimeout( function() {
+            compras_prove.style.opacity = "1"
+        }, 115)
+
         modulo_clientes.style.display = "block";
+        setTimeout( function() {
+            modulo_clientes.style.opacity = "1"
+        }, 145)
+
         modulo_empleados.style.display = "block";
+        setTimeout( function() {
+            modulo_empleados.style.opacity = "1"
+        }, 175)
+
+
 
     } else if (valor_input == "vendedor") {
+
         ventas.style.display = "block";
+        setTimeout( function() {
+            ventas.style.opacity = "1"
+        }, 25)
+
         cotizaciones.style.display = "block";
+        setTimeout( function() {
+            cotizaciones.style.opacity = "1"
+        }, 55)
+
         modulo_clientes.style.display = "block";
+        setTimeout( function() {
+            modulo_clientes.style.opacity = "1"
+        }, 85)
 
     } else if (valor_input == "almacenista") {
         productos.style.display = "block";
+        setTimeout( function() {
+            productos.style.opacity = "1"
+        }, 25)
+
         compras_prove.style.display = "block";
+        setTimeout( function() {
+            compras_prove.style.opacity = "1"
+        }, 55)
     }
     
 
