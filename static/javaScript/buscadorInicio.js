8b14feef4c674913503b39c199a186e9ac5379f3
dennis1236 <dennisecheverri46@gmail.com>
2024-03-10 20:40:34 -0500

    .(\_____I____//\\____I____/).

--- static/javaScript/buscadorInicio.js
+++ static/javaScript/buscadorInicio.js
@@ -113,6 +113,92 @@ document.addEventListener("DOMContentLoaded", function() {
 
 /* Muestra el nav laterar */
 
-/* function abrir_nav() {
-    section
-} */
+function abrir_nav() {
+    let fondo = document.getElementById('section_sombra');
+    let conten_desplegable = document.getElementById('conten_desplegable');
+    let icono = document.getElementById('conten_icono_u');
+    let nombre = document.getElementById('conten_nombre_2');
+    let lado_1 = document.getElementById('lado_1');
+    let lado_2 = document.getElementById('lado_2');
+    let btn_cerrar = document.getElementById('conten_btn_cerrar');
+
+    fondo.style.display = "block";
+    setTimeout(function() {
+        fondo.style.backgroundColor = "rgba(36, 36, 36, 0.6)";
+    },50);
+
+    setTimeout(function() {
+        conten_desplegable.style.left = "85%";
+    },400);
+
+    setTimeout(function() {
+        icono.style.opacity = "1";
+    },750);
+
+    setTimeout(function() {
+        nombre.style.opacity = "1";
+    },850);
+
+    setTimeout(function() {
+        lado_1.style.opacity = "1";
+    },950);
+
+    setTimeout(function() {
+        lado_2.style.opacity = "1";
+    },1050);
+
+    setTimeout(function() {
+        btn_cerrar.style.left = "-13%";
+    },1150);
+
+}
+
+
+function cerrar_nav() {
+    let fondo = document.getElementById('section_sombra');
+    let conten_desplegable = document.getElementById('conten_desplegable');
+    let icono = document.getElementById('conten_icono_u');
+    let nombre = document.getElementById('conten_nombre_2');
+    let lado_1 = document.getElementById('lado_1');
+    let lado_2 = document.getElementById('lado_2');
+    let btn_cerrar = document.getElementById('conten_btn_cerrar');
+
+
+    setTimeout(function() {
+        btn_cerrar.style.left = "140%";
+    },50);
+
+    setTimeout(function() {
+        lado_2.style.opacity = "0";
+    },150);
+
+    setTimeout(function() {
+        lado_1.style.opacity = "0";
+    },250);
+
+    setTimeout(function() {
+        nombre.style.opacity = "0";
+    },350);
+
+    setTimeout(function() {
+        icono.style.opacity = "0";
+    },450);
+
+    setTimeout(function() {
+        conten_desplegable.style.left = "100%";
+    },700);
+
+    setTimeout(function() {
+        fondo.style.backgroundColor = "rgba(36, 36, 36, 0.0)";
+    },1050);
+
+    setTimeout(function() {
+        fondo.style.display = "none";
+    },1410);
+
+}
+
+
+function detener_Propagacion(event) {
+    event.stopPropagation();
+}
